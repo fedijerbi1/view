884db44a699cc4afe777121c9a3863501a8be078
Fedi <jerbifedi@isimsf.u-sfax.tn>
2026-04-07 22:14:10 +0100

    Revert "Merge aymen into main"
    This reverts commit bedbd7eb7674ba7f122387fc9f82ddef2afe8fb0, reversing
    changes made to f34ca92209086b530d4b9cb26c16fd9af0659246.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import CreateMedecin from './components/create_medecin';
+=======
+import RegisterPatient from './Components/RegisterPatient';
+>>>>>>> parent of bedbd7e (Merge aymen into main)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/create_medecin" element={<CreateMedecin />} />
         </Routes>
       </BrowserRouter>
+=======
+      <RegisterPatient />
+>>>>>>> parent of bedbd7e (Merge aymen into main)
     </div>
   );
 }
-
 export default App;
