904020c23b3637a90ba2ecb6ecbe34e7f86925e9
aymen5555 <charfeddineayman760@gmail.com>
2026-04-27 18:31:46 +0100

    after merging which failed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-<<<<<<< HEAD
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Login from './components/Login';
+import CreateMedecin from './components/create_medecin'; 
+import Espaceadmin from './components/Espaceadmin';
 import RegisterPatient from './components/RegisterPatient';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/login" element={<Login />} /><Route path="/register" element={<RegisterPatient />} />
-      </Routes>
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
-import CreateMedecin from './components/create_medecin'; 
-import Espaceadmin from './components/Espaceadmin';
-import RegisterPatient from './components/RegisterPatient';
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/create_medecin" element={<CreateMedecin />} /> 
           <Route path="/espace-admin" element={<Espaceadmin />} /> 
-          <Route path="/register-patient" element={<RegisterPatient />} />
+          <Route path="/register" element={<RegisterPatient />} />
         </Routes>
-      </BrowserRouter>
->>>>>>> 7dcfe66fbc7564d5b7ad4e493e20772900f3e0cf
     </div>
   );
 }
+
 export default App;
